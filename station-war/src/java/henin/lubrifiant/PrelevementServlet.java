11e0b1e5e95c421c2d37f0389c4541d0af837c60
Henintsoa Paul <henintsoapaul@gmail.com>
2024-11-03 19:36:48 +0200

    [metier][servlet]-Gerer prelevement depuis le mobile

--- station-war/src/java/henin/lubrifiant/PrelevementServlet.java
+++ station-war/src/java/henin/lubrifiant/PrelevementServlet.java
@@ -1,6 +1,10 @@
 package henin.lubrifiant;
 
+import prelevementLub.IPrelevementLubEJB;
+import utils.EJBGetter;
+import utils.json.JsonError;
 import utils.json.JsonUtil;
+import venteLubrifiant.henin.MyPrelevementLub;
 
 import javax.naming.NamingException;
 import javax.servlet.annotation.WebServlet;
@@ -11,24 +15,28 @@ import java.io.IOException;
 @WebServlet( urlPatterns = "lubrifiant/prelevement" )
 public class PrelevementServlet extends LubrifiantServlet {
 
+    IPrelevementLubEJB prelevementLubEJB;
+
     public PrelevementServlet()
             throws NamingException {
         super();
+        prelevementLubEJB = EJBGetter.getPrelevementLubEJB();
     }
 
     @Override
-    protected void doPost( HttpServletRequest req, HttpServletResponse resp ) {
+    protected void doPost( HttpServletRequest req, HttpServletResponse resp )
+            throws IOException {
         try {
             super.setCORS( resp );
             String jsonData = JsonUtil.getFromWeb( req );
-            System.out.println( jsonData );
-
-            // todo : Mappage data
-            // todo : persist vente
+            MyPrelevementLub myPrelevementLub = this.getGson().fromJson( jsonData, MyPrelevementLub.class );
+            System.out.println( myPrelevementLub.toString() );
 
             super.dataOk( resp );
-        } catch ( IOException e ) {
-            throw new RuntimeException( e );
+            boolean isVente = prelevementLubEJB.reglerPrelevementLub( myPrelevementLub );
+        } catch ( Exception e ) {
+            resp.setStatus( HttpServletResponse.SC_INTERNAL_SERVER_ERROR );
+            resp.getWriter().write( this.gson.toJson( new JsonError( "Erreur interne: " + e.getMessage() ) ) );
         }
     }
 
